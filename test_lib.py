bb77d34f3eb2e61439941e5f53b308d1f2dd9bbd
Andre Ulrich <andre.ulrich63920@gmail.com>
2021-08-09 14:56:14 +0200

    test_lib.py und environment.yml angepasst

---
+++ test_lib.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  7 15:18:35 2021
+
+@author: andre
+"""
+def test_func():
+    print('test function executed')
+    
+if __name__ == '__main__':
+    test_func()
